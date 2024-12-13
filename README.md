ae04fedb204e9d8f44b51dd29cf7369e48985397
Gustavo Teixeira <gustavotc95@gmail.com>
2024-12-13 19:43:12 -0300

    Initial commit: Add MultiSelect component, styles, and configuration files

---
+++ README.md
@@ -0,0 +1,65 @@
+# react-multi-select-custom
+
+Um componente de seleção múltipla para React.
+
+## Instalação
+
+### npm
+
+```sh
+npm install react-multi-select-custom
+```
+
+### GitHub Packages
+
+```sh
+npm install @gustavotc95/react-multi-select-custom --registry=https://npm.pkg.github.com
+```
+
+## Uso
+
+```jsx
+import React, { useState } from "react";
+import MultiSelect from "react-multi-select-custom";
+import "react-multi-select-custom/dist/style.css";
+
+const options = [
+  { id: "1", name: "Option 1" },
+  { id: "2", name: "Option 2" },
+  { id: "3", name: "Option 3" },
+];
+
+const App = () => {
+  const [selectedValues, setSelectedValues] = useState([]);
+
+  return (
+    <MultiSelect
+      options={options}
+      selectedValues={selectedValues}
+      onChange={setSelectedValues}
+      label="Select Options"
+      id="multi-select"
+    />
+  );
+};
+
+export default App;
+```
+
+## Propriedades
+
+| Propriedade      | Tipo                           | Descrição                                   |
+| ---------------- | ------------------------------ | ------------------------------------------- |
+| `options`        | `Option[]`                     | Lista de opções disponíveis para seleção.   |
+| `selectedValues` | `string[]`                     | Lista de valores selecionados.              |
+| `onChange`       | `(selected: string[]) => void` | Função chamada quando a seleção é alterada. |
+| `label`          | `string`                       | Rótulo do campo de seleção múltipla.        |
+| `id`             | `string`                       | ID do campo de seleção múltipla.            |
+
+## Estilos
+
+Certifique-se de importar o arquivo CSS para aplicar os estilos ao componente:
+
+```jsx
+import "react-multi-select-custom/dist/style.css";
+```
---
+++ src/MultiSelect.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import "./style.css";
+
+interface Option {
+  id: string | number;
+  name: string;
+}
+
+interface MultiSelectProps {
+  options: Option[];
+  selectedValues: string[];
+  onChange: (selectedValues: string[]) => void;
+  label: string;
+  id: string;
+}
+
+const MultiSelect = ({
+  options,
+  selectedValues,
+  onChange,
+  label,
+  id,
+}: MultiSelectProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const filteredOptions = options.filter((option) =>
+    option.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (optionId: string | number) => {
+    const newSelectedValues = selectedValues.includes(optionId.toString())
+      ? selectedValues.filter((id) => id !== optionId.toString())
+      : [...selectedValues, optionId.toString()];
+    onChange(newSelectedValues);
+    setIsOpen(false);
+    setSearchTerm("");
+  };
+
+  const handleRemoveOption = (optionId: string | number) => {
+    const newSelectedValues = selectedValues.filter(
+      (id) => id !== optionId.toString()
+    );
+    onChange(newSelectedValues);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        containerRef.current !== null &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="multi-select-container" ref={containerRef}>
+      <label htmlFor={id}>{label}</label>
+      <div className="multi-select-input" onClick={handleToggle}>
+        {selectedValues.length === 0 ? (
+          <span className="placeholder">Selecione...</span>
+        ) : (
+          <div className="selected-options">
+            {selectedValues.map((value) => {
+              const option = options.find((opt) => opt.id.toString() === value);
+              return option !== null && option !== undefined ? (
+                <span key={option.id} className="selected-option">
+                  {option.name}
+                  <button
+                    onClick={() => {
+                      handleRemoveOption(option.id);
+                    }}
+                  >
+                    &times;
+                  </button>
+                </span>
+              ) : null;
+            })}
+          </div>
+        )}
+        <span className={`arrow ${isOpen ? "open" : ""}`}></span>
+      </div>
+      {isOpen && (
+        <div className="options-container">
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+            className="search-input"
+          />
+          <div className="options-list">
+            {filteredOptions.map((option) => (
+              <div
+                key={option.id}
+                className={`option ${
+                  selectedValues.includes(option.id.toString())
+                    ? "selected"
+                    : ""
+                }`}
+                onClick={() => {
+                  handleOptionClick(option.id);
+                }}
+              >
+                {option.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MultiSelect;
